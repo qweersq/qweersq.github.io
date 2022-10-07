b7b8b300167dd537cc7048e462509c496b34e7f0
Hadad Al Akbar <akbarhadadal@gmail.com>
2022-10-07 11:24:07 +0700

    add final data

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,12 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'akbarhadadal@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Sorry, cannot send messages right here. Please contact me from number or email.');
   }
 
   $contact = new PHP_Email_Form;
